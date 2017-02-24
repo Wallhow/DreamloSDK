2ccacd304940ecfb3052d8d73a370cfed63cf64d
wallhow <andrewzhokin@gmail.com>
2017-02-24 17:07:17 +0000

    Зарефакторил, добавил AdminService

--- src/main/kotlin/wallhow/dreamlo/sdk/DreamloSDK.kt
+++ src/main/kotlin/wallhow/dreamlo/sdk/DreamloSDK.kt
@@ -6,40 +6,21 @@ import org.apache.http.conn.HttpHostConnectException
 import org.apache.http.impl.client.BasicResponseHandler
 import org.apache.http.impl.client.DefaultHttpClient
 import wallhow.dreamlo.sdk.achievements.Achievements
+import wallhow.dreamlo.sdk.admin.AdminService
 import wallhow.dreamlo.sdk.leaderboard.Leaderboard
-import wallhow.dreamlo.sdk.utils.User
 import java.net.URI
-import java.util.*
 
 /**
  * Created by wallhow on 23.02.17.
  */
 
 class DreamloSDK(private val publicKey: String,private val privateKey: String) {
-    private val httpClient = DefaultHttpClient()
-    private val responseHandler = BasicResponseHandler()
-    private val httpGet = HttpGet()
-    private val gson = Gson()
-    private val host = "http://dreamlo.com/lb/"
     val leaderboard = Leaderboard()
     val achievemts = Achievements()
+    val admin = AdminService()
 
     init {
         dreamloSDK = this
-    }
-
-    @Throws(HttpHostConnectException::class)
-    private fun execute(url : String, key: String) : String {
-        httpGet.uri = URI.create("$host$key/$url")
-        return httpClient.execute(httpGet, responseHandler).toString()
-    }
-
-    private enum class Type(val url: String) {
-        add("add/"),
-        delete("delete/"),
-        clear("clear/"),
-
-        jsonGet("json-get/")
     }
 
     companion object instance {
@@ -49,7 +30,6 @@ class DreamloSDK(private val publicKey: String,private val privateKey: String) {
         private val httpClient = DefaultHttpClient()
         private val responseHandler = BasicResponseHandler()
         private val httpGet = HttpGet()
-
         private val host = "http://dreamlo.com/lb/"
 
         @Throws(HttpHostConnectException::class)
